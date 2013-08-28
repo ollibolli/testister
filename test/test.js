34235e47ba687b5706439fb23a453de9cf5667d9
Olle Svensson <bolliolle@gmail.com>
2013-08-28 23:35:01 +0200

    added support for clear scaffoled files and to run spesific test or tests in a dirrectory

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var chai = require('chai'),
+	expect = chai.expect;
+
+describe('FileSystemService',function(){
+	describe('testing',function(){
+		it('do it',function(){
+			expect(1).to.equals(1);
+		});
+	});
+});
+
+var assert = require("assert")
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
